2768ece727055d15f078f19db990891637fbfcb6
Adrian Eguez <eadepto@hotmail.com>
2018-05-23 10:39:51 -0500

    dialogos

--- app/src/main/java/com/example/usrgam/ejemplo/DialogoActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/DialogoActivity.kt
@@ -1,8 +1,11 @@
 package com.example.usrgam.ejemplo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_dialogo.*
 
@@ -14,6 +17,25 @@ class DialogoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("¿Está seguro?")
+            builder.setPositiveButton(
+                    "Aceptar",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Log.i("dialogo", "Acepto")
+                    }
+            )
+            builder.setNegativeButton(
+                    "Cancelar",
+                    null
+            )
+            val dialogo = builder.create()
+            dialogo.show()
+
+
+
+
+
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
