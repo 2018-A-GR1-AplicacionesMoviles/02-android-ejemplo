eeffd2084929cbaa64013e241662a616e3f4223a
Adrian Eguez <eadepto@hotmail.com>
2018-06-13 10:58:11 -0500

    camara

--- app/src/main/java/com/example/usrgam/ejemplo/CamaraActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/CamaraActivity.kt
@@ -11,6 +11,8 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
 import android.util.Log
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.activity_camara.*
 import java.io.File
 import java.text.SimpleDateFormat
@@ -88,13 +90,46 @@ class CamaraActivity : AppCompatActivity() {
         when (requestCode) {
             TOMAR_FOTO_REQUEST -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    val fotoActualBitmap = BitmapFactory
-                            .decodeFile(directorioActualImagen)
+                    val extras = data.extras
+                    val fotoActualBitmap = extras!!.get("data") as Bitmap
                     image_view_foto.setImageBitmap(fotoActualBitmap)
+                    obtenerInfoCodigoBarras(fotoActualBitmap)
                 }
             }
         }
     }
+
+    fun obtenerInfoCodigoBarras(bitmap: Bitmap) {
+
+        val imagenCodigo = FirebaseVisionImage
+                .fromBitmap(bitmap)
+
+
+        val detector = FirebaseVision
+                .getInstance()
+                .visionBarcodeDetector
+
+
+        val result = detector
+                .detectInImage(imagenCodigo)
+                .addOnSuccessListener { barCodes ->
+                    Log.i("info",
+                            "------- tamano del barcode ${barCodes.size}")
+                    for (barcode in barCodes) {
+                        val bounds = barcode.getBoundingBox()
+                        val corners = barcode.getCornerPoints()
+                        val rawValue = barcode.getRawValue()
+
+                        Log.i("info", "------- $bounds")
+                        Log.i("info", "------- $corners")
+                        Log.i("info", "------- $rawValue")
+                    }
+                }
+                .addOnFailureListener {
+                    Log.i("info", "------- No reconocio nada")
+                }
+    }
+
 
     companion object {
         val TOMAR_FOTO_REQUEST = 1
